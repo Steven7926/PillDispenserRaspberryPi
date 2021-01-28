c19ce3f09482f381b41cafbb4272994fe018b240
steve <steve@DESKTOP-RFPL45I>
2021-01-28 16:13:57 -0500

    New Push hella fire

--- GUI.py
+++ GUI.py
@@ -2,22 +2,51 @@ import tkinter as tk
 import json
 from GetUserData import GetUserData
 
-
 # Begin Window 
 window = tk.Tk(className = 'Magic Meds')
 user = GetUserData.userdata()
 
-greeting = tk.Label(text= "Hello " + user['FirstName'] + "! Welcome to Magic Meds!", background = "#1e3f66", foreground = "white", font = "Arial 25 bold")
+def checkSignup():
 
-greeting.place(relx = 0.5, rely = 0.5, anchor = 'center')
+	user = GetUserData.userdata()
+	if (user == 0):
+		window.destroy()
+	else:
+		window.destroy()
+		print("success")
 
-# This removes the window resiz, minimize, and close. 
-# This will only be uncommented when we have an MVP
-window.overrideredirect(1)
-window.geometry('{}x{}'.format(800, 480))
-window.configure(bg = '#1e3f66')
 
-A = tk.Button(window, text ="Continue", command = window.destroy)
-A.place(relx = 0.5, rely = 0.6, anchor = 'center')
-window.mainloop()
 
+def makeWindow():
+
+
+	if (user == 0):
+		greeting = tk.Label(text= "Hello! Welcome to Magic Meds!", background = "#1e3f66", foreground = "white", font = "Arial 20 bold")
+		greetingnext = tk.Label(text= "Please sign up on the mobile app before preceeding.", background = "#1e3f66", foreground = "white", font = "Arial 20 bold")
+		greetingafter = tk.Label(text= "Press the button below when you have signed up.", background = "#1e3f66", foreground = "white", font = "Arial 20 bold")
+		A = tk.Button(window, text ="I have signed up!", command = checkSignup)
+
+		greeting.place(relx = 0.5, rely = 0.3, anchor = 'center')
+		greetingnext.place(relx = 0.5, rely = 0.4, anchor = 'center')
+		greetingafter.place(relx = 0.5, rely = 0.5, anchor = 'center')
+
+		A.place(relx = 0.5, rely = 0.6, anchor = 'center')
+
+	else: 
+		greeting = tk.Label(text= "Hello " + user['FirstName'] + "! Welcome to Magic Meds!", background = "#1e3f66", foreground = "white", font = "Arial 25 bold")
+		A = tk.Button(window, text ="Continue", command = window.destroy)
+
+		greeting.place(relx = 0.5, rely = 0.5, anchor = 'center')
+		A.place(relx = 0.5, rely = 0.6, anchor = 'center')
+
+
+
+	# This removes the window resize, minimize, and close. 
+	# This will only be uncommented when we have an MVP
+	window.overrideredirect(1)
+	window.geometry('{}x{}'.format(800, 480))
+	window.configure(bg = '#1e3f66')
+
+	window.mainloop()
+
+makeWindow()
--- GetUserData.py
+++ GetUserData.py
@@ -4,11 +4,27 @@ import json
 
 class GetUserData:
     def userdata():
+        
+        #Product Code is hard coded value that is machine specific
+        productCode = '8902'
+
         # Make connection to DB and Cluser
         client = MongoClient('mongodb+srv://Steven:XO9V<Bpf)bGCNfKEX.6P0h!Z@cluster0.tjzfa.mongodb.net/MedMaster?retryWrites=true&w=majority')
         db = client.MedMaster
 
         # Get data from the Collection
-        user = db.Users.find_one({'ProductCode': '1234'})
+        user = db.Users.find_one({'ProductCode': productCode})
+        usercount = db.Users.find({'ProductCode': productCode}).count()
+
+        if (usercount == 0):
+            return 0
+
+        userid = str(user["_id"])
+
+        # Get the Users Caregivers and Medications
+        usersCaregivers = db.Caregivers.find({'UserId': userid})
+        usersMedication = db.Medications.find ({'UserId': userid})
+
+        print(usercount)
 
         return user
